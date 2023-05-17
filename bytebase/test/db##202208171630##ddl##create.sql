d5ae549cb1ff03017686ff4231c4f5fe42318ed2
Sam <sam.samuel@supportsages.com>
2023-05-17 11:25:47 +0530

    check 5

--- bytebase/test/db##202208171630##ddl##create.sql
+++ bytebase/test/db##202208171630##ddl##create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employe (
-	user_ids PRIMARY KEY,
+	user_ids INT PRIMARY KEY,
 	username VARCHAR ( 60 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
