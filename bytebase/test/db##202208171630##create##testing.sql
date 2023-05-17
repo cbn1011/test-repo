2139a2bc974d0f88785307b78296a47602cfdd88
Sam <sam.samuel@supportsages.com>
2023-05-17 11:09:25 +0530

    check 2

--- bytebase/test/db##202208171630##create##testing.sql
+++ bytebase/test/db##202208171630##create##testing.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employe (
-	user_i PRIMARY KEY,
+	user_ids PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
