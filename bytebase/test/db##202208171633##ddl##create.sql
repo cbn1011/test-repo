8a28932270da86587db34e1065955bd1e07ab282
Sam <sam.samuel@supportsages.com>
2023-05-18 12:40:11 +0530

    added new sql file

---
+++ bytebase/test/db##202208171633##ddl##create.sql
@@ -0,0 +1,25 @@
+CREATE TABLE emp (
+	user_ids INT PRIMARY KEY,
+	username VARCHAR ( 60 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP
+);
+CREATE TABLE jobs (
+        user_  PRIMARY KEY,
+        username VARCHAR ( 60 ) UNIQUE NOT NULL,
+        password VARCHAR ( 50 ) NOT NULL,
+        email VARCHAR ( 255 ) UNIQUE NOT NULL,
+        created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP
+);
+CREATE TABLE client (
+        user_i INT PRIMARY KEY,
+        username VARCHAR ( 60 ) UNIQUE NOT NULL,
+        password VARCHAR ( 50 ) NOT NULL,
+        email VARCHAR ( 255 ) UNIQUE NOT NULL,
+        created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP
+);
+
