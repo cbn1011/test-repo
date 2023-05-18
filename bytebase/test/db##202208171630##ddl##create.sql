6cf24c5420d311a6350713c538872552f7f1802a
Sam <sam.samuel@supportsages.com>
2023-05-18 12:03:32 +0530

    adding extra commands

--- bytebase/test/db##202208171630##ddl##create.sql
+++ bytebase/test/db##202208171630##ddl##create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE employe (
+CREATE TABLE employ (
 	user_ids INT PRIMARY KEY,
 	username VARCHAR ( 60 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
@@ -6,3 +6,20 @@ CREATE TABLE employe (
 	created_on TIMESTAMP NOT NULL,
         last_login TIMESTAMP
 );
+CREATE TABLE staff (
+        user_id PRIMARY KEY,
+        username VARCHAR ( 60 ) UNIQUE NOT NULL,
+        password VARCHAR ( 50 ) NOT NULL,
+        email VARCHAR ( 255 ) UNIQUE NOT NULL,
+        created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP
+);
+CREATE TABLE clients (
+        user_i INT PRIMARY KEY,
+        username VARCHAR ( 60 ) UNIQUE NOT NULL,
+        password VARCHAR ( 50 ) NOT NULL,
+        email VARCHAR ( 255 ) UNIQUE NOT NULL,
+        created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP
+);
+
