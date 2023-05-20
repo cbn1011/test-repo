f219139b569a686438a88d3a66ee669dd5fdb0cc
Sam <sam.samuel@supportsages.com>
2023-05-20 17:51:26 +0530

    check 2

--- bytebase/test/db##202208171634##ddl##create.sql
+++ bytebase/test/db##202208171634##ddl##create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE empp (
-	user_idl INT PRIMARY KEY,
+	user_idt INT PRIMARY KEY,
 	username VARCHAR ( 60 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
--- bytebase/test/fold/db##202208171635##ddl##alter.sql
+++ bytebase/test/fold/db##202208171635##ddl##alter.sql
@@ -1,3 +1,3 @@
 INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
-VALUES (4, 'JohnDoe', 'mypassword', 'johndoe@example.com', '2023-05-20 12:00:00', NULL);
+VALUES (5, 'JohnDoe', 'mypassword', 'johndoe@example.com', '2023-05-20 12:00:00', NULL);
 
