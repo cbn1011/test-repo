e244121b793a4f855cc9f91b3777fb5ce6962b23
Sam <sam.samuel@supportsages.com>
2023-05-20 17:45:36 +0530

    checking 1

--- bytebase/test/db##202208171633##ddl##alter.sql
+++ bytebase/test/db##202208171633##ddl##alter.sql
@@ -1,5 +1,5 @@
 CREATE TABLE em (
-	user_ids INT PRIMARY KEY,
+	user_idl INT PRIMARY KEY,
 	username VARCHAR ( 60 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
