da1b3b490fa947e6ad955412ff390b8a0c207f14
Sam <sam.samuel@supportsages.com>
2023-05-20 17:17:50 +0530

    insert command-1

--- bytebase/test/fold/db##202208171635##ddl##alter.sql
+++ bytebase/test/fold/db##202208171635##ddl##alter.sql
@@ -1,3 +1,3 @@
 INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
-VALUES (1, 'JohnDoe', 'mypassword', 'johndoe@example.com', '2023-05-20 12:00:00', NULL);
+VALUES (2, 'JohnDoe', 'mypassword', 'johndoe@example.com', '2023-05-20 12:00:00', NULL);
 
