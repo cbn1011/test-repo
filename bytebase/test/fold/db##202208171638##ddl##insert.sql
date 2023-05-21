ae4eedafa27b46558438c5580ddef13ed333b1ac
Sam <sam.samuel@supportsages.com>
2023-05-21 12:49:06 +0530

    checking 10

--- bytebase/test/fold/db##202208171638##ddl##insert.sql
+++ bytebase/test/fold/db##202208171638##ddl##insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
-VALUES (6, 'JohnDoe', 'mypassword', 'johndoe@example.com', '2023-05-20 12:00:00', NULL);
+VALUES (6, 'JohnDoe', 'mypassword', 'johndoe@example.com', '2023-05-20 12:00:00', '2023-05-21 12:00:00');
 
