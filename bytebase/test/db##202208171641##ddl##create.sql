d544ec4e54da3e0bee72bf0657cb3df729476972
Sam <sam.samuel@supportsages.com>
2023-05-21 14:26:57 +0530

    checking 03

---
+++ bytebase/test/db##202208171641##ddl##create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sages (
+	usid INT PRIMARY KEY,
+	username VARCHAR ( 60 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP
+);
+
--- bytebase/test/fold/db##202208171639##dml##insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
-VALUES (8, 'JohnD', 'mypassw', 'johnd@example.com', '2023-06-20 12:00:00', '2023-06-21 12:00:00');
-
