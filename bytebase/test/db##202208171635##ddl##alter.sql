16cbb1df60c02e3bed22aff1d8f463221d55373e
Sam <sam.samuel@supportsages.com>
2023-05-21 12:36:44 +0530

    checking 8

--- bytebase/test/db##202208171635##ddl##alter.sql
+++ bytebase/test/db##202208171635##ddl##alter.sql
@@ -7,7 +7,7 @@ CREATE TABLE em (
         last_login TIMESTAMP
 );
 CREATE TABLE jo (
-        user_   PRIMARY KEY,
+        user_ INT  PRIMARY KEY,
         username VARCHAR ( 60 ) UNIQUE NOT NULL,
         password VARCHAR ( 50 ) NOT NULL,
         email VARCHAR ( 255 ) UNIQUE NOT NULL,
