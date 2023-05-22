8c42b4c6c5eb4aab0f78a328b6f7f588e049292f
Sam <sam.samuel@supportsages.com>
2023-05-22 16:49:40 +0530

    checking 002

--- bytebase/test/fold/db##202208171649##dml##insert.sql
+++ bytebase/test/fold/db##202208171649##dml##insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO accounts (user_ids, username, password, email, created_on, last_login)
-VALUES (10, 'SamSam', 'myp', 'cbn@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
+VALUES (12, 'SamS', 'myp', 'cbn@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
