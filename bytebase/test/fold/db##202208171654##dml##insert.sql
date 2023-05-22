34183fa5a8ec6848d4d5d239eae69b5090a691dd
Sam <sam.samuel@supportsages.com>
2023-05-22 17:17:34 +0530

    checking 007

---
+++ bytebase/test/fold/db##202208171654##dml##insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
+VALUES (18, 'SamSamulsupp', 'mypss12', 'cbl@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
--- bytebase/test/folder1/db##202208171650##dml##insert.sql
+++ bytebase/test/folder1/db##202208171650##dml##insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO job (user_ids, username, password, email, created_on, last_login)
-VALUES (15, 'Adolfo', 'mypssw', 'cbn@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
+INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
+VALUES (20, 'Adolfo', 'mypssw1', 'adl@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
