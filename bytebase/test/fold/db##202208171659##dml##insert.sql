217a2bc74aebd30c36da47523a19f54932378c32
Sam <sam.samuel@supportsages.com>
2023-05-22 18:27:32 +0530

    checking 0001

---
+++ bytebase/test/fold/db##202208171659##dml##insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
+VALUES (19, 'SamSamulsupport', 'mypss1234', 'cbz@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
+
+DELETE FROM employe;
+
+INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
+VALUES (25, 'Cbn', 'qwerty', 'cbncb@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
