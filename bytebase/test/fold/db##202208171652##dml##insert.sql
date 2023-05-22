7f0dcc4d63d8728751018ce01d57d23af7de8441
Sam <sam.samuel@supportsages.com>
2023-05-22 17:13:57 +0530

    checking 014

--- bytebase/test/fold/db##202208171652##dml##insert.sql
+++ bytebase/test/fold/db##202208171652##dml##insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
-VALUES (17, 'SamSamu', 'mypss1', 'cb@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
+VALUES (17, 'SamSamul', 'mypss1', 'cbiexample.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
