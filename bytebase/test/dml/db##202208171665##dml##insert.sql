57aa0634759d02ee150606338bc723b596baa135
Sam <sam.samuel@supportsages.com>
2023-05-23 15:40:18 +0530

    checking CICD#4

--- bytebase/test/dml/db##202208171665##dml##insert.sql
+++ bytebase/test/dml/db##202208171665##dml##insert.sql
@@ -1,8 +1,6 @@
 INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
 VALUES (25, 'AdolfoT', 'mypasssw1', 'adlt@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
 
-DELETE FROM employe;
-
 INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
 VALUES (25, 'AdolfoT', 'mypasssw1', 'adlt@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
 
