8a92ccf19442ad23730d16b03afda098a3978036
Sam <sam.samuel@supportsages.com>
2023-05-23 15:34:12 +0530

    checking the CICD1

--- bytebase/test/folder1/db##202208171643##ddl##alter.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE sages (
-	useid INT PRIMARY KEY,
-	username VARCHAR ( 60 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL,
-	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-	created_on TIMESTAMP NOT NULL,
-        last_login TIMESTAMP
-);
