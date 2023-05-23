f08d902028590d0cebb666a393293415e7c52c11
Sam <sam.samuel@supportsages.com>
2023-05-23 16:00:17 +0530

    checking cicd#02

--- bytebase/test/dml/db##202208171668##dml##insert.sql
+++ bytebase/test/dml/db##202208171668##dml##insert.sql
@@ -3,3 +3,6 @@ VALUES (34, 'Mikk', 'mypss1mikk', 'mikee@example.com', '2023-06-20 12:00:00', '2
 
 INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
 VALUES (31, 'Ryan', 'mypssw1Ryan', 'Ryan@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
+
+INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
+VALUES (35, 'Ryann', 'mypssw1rRyan', 'Ryan1@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
