48a62314abba82e030590601194d97a997ef335b
Sam <sam.samuel@supportsages.com>
2023-05-23 15:42:46 +0530

    check CICd#6

---
+++ bytebase/test/dml/db##202208171667##dml##insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO employe (user_ids, username, password, email, created_on, last_login)
+VALUES (30, 'Mike', 'mypssw1mike', 'mike@example.com', '2023-06-20 12:00:00', '2023-06-23 12:00:00');
